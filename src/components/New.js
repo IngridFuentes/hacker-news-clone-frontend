b0e032df370e5933e3b1ed7a827194d6fd6c54ab
Ingrid04 <ingridfmelgar@gmail.com>
2020-09-10 15:35:45 -0400

    firebase

--- src/components/New.js
+++ src/components/New.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import functions from '../functions/index';
 
-const New = () => {
+// const New = () => {
+
+    const New = event =>{
+        const callFirebaseFunction = event => {
+            const callableReturnMessage = firebase.functions().httpsCallable('returnMessage');
+        
+            callableReturnMessage().then((result) => {
+              console.log(result.data.output);
+            }).catch((error) => {
+              console.log(`error: ${JSON.stringify(error)}`);
+            });
+        }
+        // var output = functions.updateUpVote()
+        // console.log(output)
+    
 
     return(
         <div> NEWSSSSSSS</div>
